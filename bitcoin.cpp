39b51b1b11d06961c7ae94c4b9f1cfd136f89b64
tohsnoom <moonshot@phore.io>
2019-04-04 15:56:19 -0500

    Updated for latest Phore release

--- bitcoin.cpp
+++ bitcoin.cpp
@@ -78,7 +78,7 @@ class CNode {
     CAddress me(CService("0.0.0.0"));
     BeginMessage("version");
     int nBestHeight = GetRequireHeight();
-    string ver = "/phore-seeder:0.01/";
+    string ver = "/phore-seeder:0.02/";
     vSend << PROTOCOL_VERSION << nLocalServices << nTime << you << me << nLocalNonce << ver << nBestHeight;
     EndMessage();
   }
--- db.h
+++ db.h
@@ -12,11 +12,11 @@
 
 #define MIN_RETRY 1000
 
-#define REQUIRE_VERSION 70001
+#define REQUIRE_VERSION 70007
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 1 : 200000;
+    return testnet ? 1 : 806492;
 }
 
 std::string static inline ToString(const CService &ip) {
